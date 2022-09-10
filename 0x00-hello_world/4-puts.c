d11458217172d19ef1f98b20928ccb363eccfbf7
Udyernest <uduakernes2@gmail.com>
2022-09-10 11:49:59 -0700

    program that print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
 /**
- *
- *  * Description: main - prints out a string to stdout
- *
- *   *
- *
- *    * Return: 0 if successful
- *
- *     */
-
+* Description: main - prints out a string to stdout
+*
+*Return: 0 if successful
+*/
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
 
