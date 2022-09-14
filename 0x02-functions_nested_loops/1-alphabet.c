6a82316d1644129ef8d82271376e0ed4b95d6a21
Udyernest <uduakernes2@gmail.com>
2022-09-14 14:51:53 -0700

    function that print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet - Print alphabets in small letters.
+*
+* Description: Print out a string.
+*
+* Return: 0 Always Successful
+*/
+
+void print_alphabet(void)
+{
+char i = 'a';
+
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+
+_putchar('\n');
+}
